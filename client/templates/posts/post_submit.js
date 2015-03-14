a252fc807ba72e17c49c2b2c2dfaba38755f4d2a
Matt Gale <matt@littleball.co.uk>
2015-03-14 11:19:32 +0000

    Added error checking to post submit

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -1,4 +1,5 @@
 Template.postSubmit.events({
+
   'submit form': function (e) {
     e.preventDefault();
 
@@ -7,7 +8,22 @@ Template.postSubmit.events({
       title: $(e.target).find('[name=title]').val()
     };
 
-    post._id = Posts.insert(post);
-    Router.go('postPage', post);
+    Meteor.call('postInsert', post, function (error, result) {
+
+      // display the error to the user
+      if (error) {
+        return alert(error.reason);
+      }
+
+      // show this result buit route anyway
+      if (result.postExists) {
+        alert('This link has alreay been posted');
+      }
+
+      Router.go('postPage', {
+        _id: result._id
+      });
+    });
   }
+
 });
