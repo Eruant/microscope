b13936f3d25adc1d947a2596f914efe4ea5a2fc4
Matt Gale <matt@littleball.co.uk>
2015-03-23 13:44:08 +0000

    Added animation to voting

---
+++ client/templates/posts/post_list.js
@@ -0,0 +1,51 @@
+Template.postsList.rendered = function () {
+
+  this.find('.wrapper')._uihooks = {
+
+    moveElement: function (node, next) {
+
+      var $node = $(node),
+        $next = $(next),
+        oldTop = $node.offset().top,
+        height = $node.outerHeight(true),
+
+        // find elements in between next and node
+        $inBetween = $next.nextUntil(node);
+
+      if ($inBetween.length === 0) {
+        $inBetween = $node.nextUntil(next);
+      }
+
+      // now put node in place
+      $node.insertBefore(next);
+
+      // measure new top
+      var newTop = $node.offset().top;
+
+      // move node back to wher is was before
+      $node
+        .removeClass('animate')
+        .css('top', oldTop - newTop);
+
+      // push every other element down (or up) to put them back
+      $inBetween
+        .removeClass('animate')
+        .css('top', oldTop < newTop ? height : height * -1);
+
+      // force a redraw
+      $node.offset();
+
+      // reset everything to 0, animated
+      $node
+        .addClass('animate')
+        .css('top', 0);
+
+      $inBetween
+        .addClass('animate')
+        .css('top', 0);
+
+    }
+
+  };
+
+};
